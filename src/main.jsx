876ff7b31022f887a207de6c80c6451ad2a395cf
mateo vincitorio <mateovincitorio@gmail.com>
2026-04-16 12:14:19 -0300

    segundo commit sparium

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx'
 import { PayPalScriptProvider } from '@paypal/react-paypal-js'
-import dotenv from 'dotenv'
-dotenv.config()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PayPalScriptProvider
       options={{
-        'client-id': process.env['client-id'],
+        'client-id': import.meta.env.VITE_PAYPAL_CLIENT_ID || 'test',
         components: 'buttons',
         currency: 'USD',
       }}
